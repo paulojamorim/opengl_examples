2f7b97d7fff951936feb8ca605895ec3e8534e14
paulojamorim <paulojamorim@gmail.com>
2012-10-31 09:51:26 -0200

    Updated files

--- rectangle.cpp
+++ rectangle.cpp
@@ -25,7 +25,7 @@ void display(void)
 
    glClearColor(0.0, 0.0, 0.0, 0.0);
    glClear(GL_COLOR_BUFFER_BIT);
-   glColor3f(1.0, 1.0, 1.0);
+   glColor3f(1.0, 1.0, 0);
    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); 
    glBegin(GL_POLYGON);
       glVertex2f(-0.5, -0.5);
