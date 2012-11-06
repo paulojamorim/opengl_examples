379e0099823ebaabd7c28ffda271e1192d8179be
paulojamorim <paulojamorim@gmail.com>
2012-11-06 15:27:18 -0200

    Initialize virtual trackball implementation

--- teapot.cpp
+++ teapot.cpp
@@ -1,8 +1,9 @@
 #include <GL/glut.h>
 #include <iostream>
 
- using namespace std;
+using namespace std;
 
+float* ViewPosTo3D(float *p);
 void Desenha(void);
 void AlteraTamanhoJanela(GLsizei w, GLsizei h);
 void EspecificaParametrosVisualizacao(void);
@@ -11,6 +12,26 @@ void GerenciaMouseMove(int x, int y);
 void Inicializa(void);
 
 GLfloat angle, fAspect;
+bool tracking_mouse = false;
+float velocity = 0.0;
+int last_time = 0;
+int win_width, win_height;
+
+
+float* ViewPosTo3D(float *p)
+{
+    float px, py;
+    px = p[0];
+    py = p[1];
+
+    float point3d[3];
+
+    point3d[0] = 2.0 * px / win_width - 1.0;
+    point3d[1] = 1.0 - 2.0 * py / win_height;
+    point3d[2] = 0.0;
+ 
+    return point3d;
+}
 
 void Desenha(void)
 {
@@ -27,6 +48,9 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
     
     glViewport(0, 0, w, h);
     fAspect = (GLfloat)w / (GLfloat)h;
+    
+    win_width = w;
+    win_height = h;
 
     EspecificaParametrosVisualizacao();
 }
@@ -44,25 +68,49 @@ void EspecificaParametrosVisualizacao(void)
 
 void GerenciaMouseMove(int x, int y)
 {
-    cout << x << " " << y << endl;
+    float b[2];
+    b[0] = x;
+    b[1] = y;
+
+    float *r;
+    r = ViewPosTo3D(b);
+    cout << r[0] << " " << r[1] <<  " " << r[2] << endl;
 }
 
 void GerenciaMouse(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON)
+    {
         if (state == GLUT_DOWN)
         {
-            cout << x << "  "<< y << endl;
+            /*cout << x << "  "<< y << endl;
             if (angle >= 10)
-                angle -= 5;
+                angle -= 5;*/
+            
+            tracking_mouse = true;
+            cout << "true" << endl;
         }
 
+        if (state == GLUT_UP)
+        {
+            tracking_mouse = false;
+            cout << "false" << endl;
+        }
+
+    }
+
+    /*
     if (button == GLUT_RIGHT_BUTTON)
         if (state == GLUT_DOWN)
         {
             if (angle <= 130)
                 angle += 5;
         }
+    */
+    
+
+    //cout << x << "   " << y << endl;
+
 
     EspecificaParametrosVisualizacao();
     glutPostRedisplay();
@@ -78,7 +126,11 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv); 
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
     glutInitWindowSize(350, 300);
+    win_width = 350;
+    win_height = 300;
+
     glutCreateWindow("Teapot");
     glutDisplayFunc(Desenha);
     glutReshapeFunc(AlteraTamanhoJanela);
